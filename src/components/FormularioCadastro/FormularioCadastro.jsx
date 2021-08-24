3b682ad7a16af1a6fc1552c6111cd23efc36cd31
w0lf-46 <lucaspsrrio@gmail.com>
2021-08-24 16:52:55 -0300

    Atualização de files

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import "./style.css";
+class FormularioCadastro extends Component {
+  constructor(props) {
+    super(props);
+
+    this.titulo = "";
+    this.nota = "";
+  }
+
+  _handleMudancaTitulo(evento) {
+    evento.stopPropagation();
+    this.titulo = evento.target.value;
+  }
+
+  _handleMudancaNota(evento) {
+    evento.stopPropagation();
+    this.nota = evento.target.value;
+  }
+
+  _criarNota(evento) {
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo, this.nota);
+  }
+
+  render() {
+    return (
+      <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange={this._handleMudancaTitulo.bind(this)}
+        />
+        <textarea
+          rows={15}
+          placeholder="Escreva a sua nota..."
+          className="form-cadastro_input"
+          onChange={this._handleMudancaNota.bind(this)}
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar Nota
+        </button>
+      </form>
+    );
+  }
+}
+
+export default FormularioCadastro;
